e54f5a7296617f5669ee6b93e485815c04778671
Molkobahn <stoffeosterhof@gmail.com>
2024-10-05 13:30:38 +0200

    added letter count

--- main.py
+++ main.py
@@ -2,8 +2,19 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
+    lowered_text = text.lower()
+    num_char = get_num_char(lowered_text)
+    print(f"{num_char}")
 
+
+def get_num_char(lowered_text):
+    char_dict = {}
+    for letter in lowered_text:
+        if letter in char_dict:
+            char_dict[letter] += 1
+        else:
+            char_dict[letter] = 1
+    return char_dict
 
 def get_num_words(text):
     words = text.split()
