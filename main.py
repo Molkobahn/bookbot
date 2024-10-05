5d94815ef191974c72716219545e78fae73ecdb9
Molkobahn <stoffeosterhof@gmail.com>
2024-10-05 17:29:34 +0200

    formatted report

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    num_words = get_num_words(text)
- 
-    char_count = count_characters(text, count_all=True)
-    print("Character counts:")
-    print(char_count)
+    num_words = count_words_in_text(text)
+    char_count = count_characters(text, count_all=False)
+    character_dict = dict_list(char_count)
+    character_dict.sort(reverse=True, key=sort_on)
+    print_report(num_words, character_dict)
+    
 
+def sort_on(character_dict):
+    return character_dict["num"]
+
+def dict_list(char_count):
+    list_of_dict = []
+    for character in char_count:
+        num = char_count[character]
+        new_dict = {"character" : character, "num" : num}
+        list_of_dict.append(new_dict)
+    return list_of_dict
 
 def count_characters(text, count_all=False):
     char_dict = {}
@@ -18,7 +29,7 @@ def count_characters(text, count_all=False):
                 char_dict[letter] = 1
     return char_dict
 
-def get_num_words(text):
+def count_words_in_text(text):
     words = text.split()
     return len(words)
           
@@ -27,5 +38,11 @@ def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-
+def print_report(num_words, character_dict):
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{num_words} words found in the document")
+    for x in character_dict:
+        print(f"The '{x['character']}' character was found {x['num']} times")
+    print("--- End report ---")
+    
 main()
