0283197c01d1ba873ed5badf07fd06d9bbb159e4
Molkobahn <stoffeosterhof@gmail.com>
2025-02-25 20:32:35 +0100

    cleaned up code

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
+import sys
+from stats import count_words_in_text
+
 def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
+    text = get_book_text(sys.argv[1])
     num_words = count_words_in_text(text)
     char_count = count_characters(text, count_all=False)
     character_dict = dict_list(char_count)
     character_dict.sort(reverse=True, key=sort_on)
     print_report(num_words, character_dict)
     
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
 def get_book_text(path):
     with open(path) as f:
         return f.read()
-
-def count_words_in_text(text):
-    words = text.split()
-    return len(words)
 
 def count_characters(text, count_all=False):
     char_dict = {}
@@ -37,7 +39,7 @@ def sort_on(character_dict):
     return character_dict["num"]
 
 def print_report(num_words, character_dict):
-    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"--- Begin report of {sys.argv[1]} ---")
     print(f"{num_words} words found in the document")
     for x in character_dict:
         print(f"The '{x['character']}' character was found {x['num']} times")
---
+++ stats.py
@@ -0,0 +1,3 @@
+def count_words_in_text(text):
+    words = text.split()
+    return len(words)
